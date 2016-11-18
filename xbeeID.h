f2c77524ba1bb8e9e67b58fa05fc59d0bc129e88
Ipl-Abe <crowsky7275@gmail.com>
2016-11-18 14:05:34 +0900

    Create xbeeID.h

---
+++ xbeeID.h
@@ -0,0 +1,120 @@
+
+
+//zigbee名とアドレスの対応
+
+
+//PAN1_C
+#define PAN1_C_ID a
+#define PAN1_C_SH 0x0013a200
+#define PAN1_C_SL 0x409eaf0d
+
+/*
+//PAN1_R1
+#define PAN1_R1_ID A
+#define PAN1_R1_SH 0x0013a200
+#define PAN1_R1_SL 0x409eaef3
+
+//PAN1_R2
+#define PAN1_R2_ID B
+#define PAN1_R2_SH 0x0013a200
+#define PAN1_R2_SL 0x40993719
+
+//PAN1_R3
+#define PAN1_R3_ID C
+#define PAN1_R3_SH 0x0013a200
+#define PAN1_R3_SL 0x409eaeeb
+
+//PAN1_R4
+#define PAN1_R4_ID D
+#define PAN1_R4_SH 0x0013a200
+#define PAN1_R4_SL 0x409eaf0e
+
+//PAN1_R5
+#define PAN1_R5_ID E
+#define PAN1_R5_SH 0x0013a200
+#define PAN1_R5_SL 0x409eaef9
+
+//PAN1_R6
+#define PAN1_R6_ID F
+#define PAN1_R6_SH 0x0013a200
+#define PAN1_R6_SL 0x409eaef1
+
+//PAN1_R7
+#define PAN1_R7_ID G
+#define PAN1_R7_SH 0x0013a200
+#define PAN1_R7_SL 0x409eaf04
+
+//PAN1_R8
+#define PAN1_R8_ID H
+#define PAN1_R8_SH 0x0013a200
+#define PAN1_R8_SL 0x409eaf06
+
+//PAN1_R9
+#define PAN1_R9_ID I
+#define PAN1_R9_SH 0x0013a200
+#define PAN1_R9_SL 0x409eaf01
+*/
+
+
+//robot
+
+// 1
+#define PAN1_R1_SH 0x0013a200
+#define PAN1_R1_SL 0x40993787
+
+// 2
+#define PAN1_R2_SH 0x0013a200
+#define PAN1_R2_SL 0x4099371a
+
+// 3
+#define PAN1_R3_SH 0x0013a200
+#define PAN1_R3_SL 0x409936ff
+
+// 4
+#define PAN1_R4_SH 0x0013a200
+#define PAN1_R4_SL 0x40993704
+
+// 5
+#define PAN1_R5_SH 0x0013a200
+#define PAN1_R5_SL 0x409eaef7
+
+// 6
+#define PAN1_R6_SH 0x0013a200
+#define PAN1_R6_SL 0x40993703
+
+// 7
+#define PAN1_R7_SH 0x0013a200
+#define PAN1_R7_SL 0x40993701
+
+// 8
+#define PAN1_R8_SH 0x0013a200
+#define PAN1_R8_SL 0x40993716
+
+// 9 
+#define PAN1_R9_SH 0x0013a200
+#define PAN1_R9_SL 0x40993715
+
+// 10
+#define PAN1_R10_SH 0x0013a200
+#define PAN1_R10_SL 0x40993708
+
+// 11
+#define PAN1_R11_SH 0x0013a200
+#define PAN1_R11_SL 0x4099377a
+
+// 12
+#define PAN1_R12_SH 0x0013a200
+#define PAN1_R12_SL 0x40993707
+
+// 13
+#define PAN1_R13_SH 0x0013a200
+#define PAN1_R13_SL 0x40993709
+
+// 14
+#define PAN1_R14_SH 0x0013a200
+#define PAN1_R14_SL 0x40993713
+
+// 15
+#define PAN1_R15_SH 0x0013a200
+#define PAN1_R15_SL 0x40993717
+
